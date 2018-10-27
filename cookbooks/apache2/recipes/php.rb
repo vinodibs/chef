8008032192dc53998682c96b86f876c03b9c0d06
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-10-27 16:00:57 +0530

    Update php.rb

--- cookbooks/apache2/recipes/php.rb
+++ cookbooks/apache2/recipes/php.rb
@@ -3,52 +3,66 @@ file "/var/www/html/info.php" do
   action :create
 end
 
-package "php7.0" do
+package "php-curl" do
   action :install
 end
 
-package "php7.0-mysql" do
+package "php-mysql" do
   action :install
 end
 
-package "php7.0-cli" do
+package "php-cli" do
   action :install
 end
 
-package "php7.0-cgi" do
+package "php-cgi" do
   action :install
 end
 
-package "php7.0-gd" do
+package "php-gd" do
   action :install
 end
 
-package "libapache2-mod-php7.0" do
+package "libapache2-mod-php" do
   action :install
 end
 
-
-
-package "php7.0-dba" do
+package "php-dba" do
   action :install
 end
 
-package "php7.0-curl" do
+package "php-curl" do
   action :install
 end
 
-package "php7.0-mbstring" do
+package "php-mbstring" do
   action :install
 end
 
-package "php7.0-bz2" do
+package "php-bz2" do
   action :install
 end
 
-package "php7.0-zip" do
+package "php-zip" do
   action :install
 end
 
-package "php7.0-odbc" do
+package "php-odbc" do
+  action :install
+end
+
+package "php-pgsql" do
+  action :install
+end
+
+package "php-sqlite3" do
+  action :install
+end
+
+package "php-ldap" do
+  action :install
+end
+
+package "php-imap" do
   action :install
 end
